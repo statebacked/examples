0f66736b76252ad5239b3b1fdc599254ba6ee794
Adam Berger <adam@simplystated.dev>
2023-07-27 15:57:19 -0400

    feat: support test time

--- email-automation/src/duration.ts
+++ email-automation/src/duration.ts
@@ -1,3 +1,14 @@
-export const days = (nDays: number) => nDays * 24 * 60 * 60 * 1000;
+// if we don't want to wait around for days, we can use test time to speed things up.
+// days become minutes, minutes become seconds.
+const useTestTime = true;
 
-export const minutes = (nMinutes: number) => nMinutes * 60 * 1000;
+const actualDays = (nDays: number) => nDays * 24 * 60 * 60 * 1000;
+
+const actualMinutes = (nMinutes: number) => nMinutes * 60 * 1000;
+
+const actualSeconds = (nSeconds: number) => nSeconds * 1000;
+
+export const days = (nDays: number) => useTestTime ? actualMinutes(nDays) : actualDays(nDays);
+
+export const minutes = (nMinutes: number) => useTestTime ? actualSeconds(nMinutes) : actualMinutes(nMinutes);
+
