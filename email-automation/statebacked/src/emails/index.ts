c6cba92b0a401c0b1d310beb5a357148249aac8c
Adam Berger <adam@simplystated.dev>
2023-07-31 17:55:05 -0400

    feat: update email content

--- email-automation/statebacked/src/emails/index.ts
+++ email-automation/statebacked/src/emails/index.ts
@@ -32,8 +32,11 @@ export const sendEmail = async (emailAddress: string, emailType: EmailType) => {
     body: JSON.stringify({
       to: emailAddress,
       from: "test@statebacked.dev",
-      subject: `[email automation] ${emailType}`,
-      html: `<p>Hi ${emailAddress},</p><p>This is a test email for ${emailType}.</p>`,
+      subject: `[State Backed email automation example] ${emailType}`,
+      html: `<p>Hi ${emailAddress},</p>
+      <p>This is a test email for ${emailType}.</p>
+      <p><a href="https://statebacked.dev">State Backed</a> is an XState backend as a service. If you're interested, try out the generous free tier.</p>
+      <p>This emal was sent because you signed up for the State Backed <a href="https://examples-state-backed.vercel.app/">email automation example</a></p>`,
     }),
   });
 };
